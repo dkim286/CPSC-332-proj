2f3ec4c2ee96acb2518956001464ceeaa431dd7a
A <dkim286@csu.fullerton.edu>
2020-05-01 14:06:38 -0700

    select unique prescriptions per person only

--- extracred.sql
+++ extracred.sql
@@ -1,12 +1,19 @@
 DROP PROCEDURE IF EXISTS EXTRACRED;
 CREATE PROCEDURE EXTRACRED ()
-  SELECT COUNT(*) as num, PRESCRIPTION.prescriptionName
-  FROM PRESCRIPTION, PVISITDESCRIPTION, PATIENTVISIT, PATIENT, PERSON
-  WHERE PRESCRIPTION.prescriptionID = PVISITDESCRIPTION.prescriptionID
-    AND PVISITDESCRIPTION.visitID = PATIENTVISIT.visitID
-    AND PATIENTVISIT.patientID = PATIENT.patientID
-    AND PATIENT.personID = PERSON.personID
-	AND PERSON.city = 'Fullerton'
-  GROUP BY PRESCRIPTION.prescriptionName
+  SELECT COUNT(*) as num, 
+         PRES.prescriptionName
+    FROM (SELECT DISTINCT PRESCRIPTION.prescriptionName, 
+		                  PERSON.personID
+            FROM PRESCRIPTION, 
+			     PVISITDESCRIPTION, 
+				 PATIENTVISIT, 
+				 PATIENT, 
+				 PERSON
+           WHERE PRESCRIPTION.prescriptionID = PVISITDESCRIPTION.prescriptionID
+             AND PVISITDESCRIPTION.visitID = PATIENTVISIT.visitID
+             AND PATIENTVISIT.patientID = PATIENT.patientID
+             AND PATIENT.personID = PERSON.personID
+             AND PERSON.city = 'Fullerton') as PRES
+  GROUP BY PRES.prescriptionName
   ORDER BY num DESC;
 
