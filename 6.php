abfa2e1f77626f2e9bb3298c270febd47c4885da
patrickmahoney <patrick.mahoney@csu.fullerton.edu>
2020-05-06 15:16:32 -0700

    added drop table and select

--- 6.php
+++ 6.php
@@ -43,31 +43,36 @@ else
 
 // display Audit
 if ($result = $conn->query("
+                           DROP TABLE IF EXISTS AUDIT;
                            CREATE TABLE AUDIT (
-                                               dFirstName varchar(10) not null,
-                                               tableAction char(6) not null,
-                                               specialtyName varchar(50) not null,
-                                               modDate date not null
-                                               );
-    
-       
+                               dFirstName varchar(10) not null,
+                               tableAction char(6) not null,
+                               specialtyName varchar(50) not null,
+                               modDate date not null
+                            );
+                                                      
+                           DROP TRIGGER IF EXISTS SpecialtyToAudit;
                            CREATE TRIGGER SpecialtyToAudit
                            AFTER INSERT ON DOCTORSPECIALTY for each row
-                           INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
-                           VALUES ((SELECT firstName FROM PERSON where personID =
-                                    (SELECT personID from DOCTOR where DoctorID = new.doctorID)), 'INSERT',
-                                (SELECT specialtyName from SPECIALTY where specialtyID =
-                                    (SELECT specialtyID from DOCTORSPECIALTY where specialtyID = New.specialtyID)),
-                                        CURRENT_TIMESTAMP);
-  
-                            CREATE TRIGGER SpecialtyToAudit2
-                            AFTER UPDATE ON DOCTORSPECIALTY for each row
-                                INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
-                                VALUES ((SELECT firstName FROM PERSON where personID =
-                                            (SELECT personID from DOCTOR where DoctorID = new.doctorID)), 'UPDATE',
-                                        (SELECT specialtyName from SPECIALTY where specialtyID =
-                                            (SELECT specialtyID from DOCTORSPECIALTY where specialtyID = New.specialtyID)),
-                                        CURRENT_TIMESTAMP);"))
+                               INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
+                               VALUES ((SELECT firstName FROM PERSON where personID =
+                                           (SELECT personID from DOCTOR where DoctorID = new.doctorID)), 'INSERT',
+                                       (SELECT specialtyName from SPECIALTY where specialtyID =
+                                           (SELECT specialtyID from DOCTORSPECIALTY where specialtyID = New.specialtyID)),
+                                       CURRENT_TIMESTAMP);
+                             
+                           DROP TRIGGER IF EXISTS SpecialtyToAudit2;
+                           CREATE TRIGGER SpecialtyToAudit2
+                           AFTER UPDATE ON DOCTORSPECIALTY for each row
+                               INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
+                               VALUES ((SELECT firstName FROM PERSON where personID =
+                                           (SELECT personID from DOCTOR where DoctorID = new.doctorID)), 'UPDATE',
+                                       (SELECT specialtyName from SPECIALTY where specialtyID =
+                                           (SELECT specialtyID from DOCTORSPECIALTY where specialtyID = New.specialtyID)),
+                                       CURRENT_TIMESTAMP);
+
+                           SELECT *
+                           FROM AUDIT;"))
 {
     print "<p class='lead'>trigger on the DoctorSpeciality so that every time a doctor specialty is updated or added, a new entry is made in the audit table.</p>";
     print "<table class='table table-bordered'>
@@ -86,9 +91,9 @@ if ($result = $conn->query("
         print "\n";
         print "<tr>
             <td>$row[dFirstName] </td>
-            <td> $row[tableAction]  </td>
+            <td>$row[tableAction] </td>
             <td>$row[specialtyName] </td>
-            <td> $row[modDate]  </td>
+            <td>$row[modDate] </td>
             </tr>
         ";
     }
--- 6.sql
+++ 6.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS AUDIT;
 CREATE TABLE AUDIT (
-  dFirstName varchar(10) not null,
-  tableAction char(6) not null,
-  specialtyName varchar(50) not null,
-  modDate date not null
+	dFirstName varchar(10) not null,
+	tableAction char(6) not null,
+	specialtyName varchar(50) not null,
+	modDate date not null
 );
-    
-       
+                           
+DROP TRIGGER IF EXISTS SpecialtyToAudit;       
 CREATE TRIGGER SpecialtyToAudit
 AFTER INSERT ON DOCTORSPECIALTY for each row
     INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
@@ -16,6 +16,7 @@ AFTER INSERT ON DOCTORSPECIALTY for each row
 				(SELECT specialtyID from DOCTORSPECIALTY where specialtyID = New.specialtyID)), 
             CURRENT_TIMESTAMP);
   
+DROP TRIGGER IF EXISTS SpecialtyToAudit2;   
 CREATE TRIGGER SpecialtyToAudit2
 AFTER UPDATE ON DOCTORSPECIALTY for each row
     INSERT INTO AUDIT (dFirstName, tableAction, specialtyName, modDate)
